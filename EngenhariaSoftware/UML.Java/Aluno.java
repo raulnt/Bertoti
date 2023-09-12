70205039c0e9504cbb6cda890d8a7f4d6e1488f9
Raul Neto <rjbnetto@gmail.com>
2023-09-12 15:47:32 -0300

    Update Java

--- EngenhariaSoftware/UML.Java/Aluno.java
+++ EngenhariaSoftware/UML.Java/Aluno.java
@@ -1,22 +1,3 @@
 public class Aluno {
     
-    private String nome;
-    private int idade;
-    private String plano;
-
-    public Aluno(String nome, int idade, String plano) {
-        this.nome = nome;
-        this.idade = idade;
-        this.plano = plano;
-    }
-
-    public void mostrarDetalhes() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
-        System.out.println("Plano: " + plano);
-    }
-
-    public void chamarInstrutor() {
-        System.out.println("Chamando instrutor...");
-    }
 }
