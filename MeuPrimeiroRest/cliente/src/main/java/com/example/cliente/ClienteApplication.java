8fcce73785af763a7b6c2c14b7543ecbbd66a699
Raul Neto <rjbnetto@gmail.com>
2024-05-23 00:31:21 -0300

    :feat: Meu Primeiro Rest

---
+++ MeuPrimeiroRest/cliente/src/main/java/com/example/cliente/ClienteApplication.java
@@ -0,0 +1,23 @@
+package com.example.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ui.Model;
+import org.modelmapper.ModelMapper;
+
+@SpringBootApplication
+public class ClienteApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setSkipNullEnabled(true);
+		return modelMapper;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApplication.class, args);
+	}
+
+}
